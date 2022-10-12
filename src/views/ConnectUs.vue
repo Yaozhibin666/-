c2c38836c4ec8367e41407de60590941dfa9d1e6
Yaozhibin666 <1655611575@qq.com>
2022-10-12 09:18:12 +0800

    :bento:添加页面

---
+++ src/views/ConnectUs.vue
@@ -0,0 +1,14 @@
+<template>
+    <ConnectMain></ConnectMain>
+    <Footer></Footer>
+</template>
+<script>
+import ConnectMain from '../Layout/components/Main/ConnectMain.vue';
+import Footer from '../Layout/components/Footer/Footer.vue';
+export default {
+    components: { ConnectMain, Footer }
+}
+</script>
+<style lang="css" scoped>
+    
+</style>
